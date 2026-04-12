443e5879086111dbecab584bd99456c1e189f1e2
Nate Stott <nate.stott@pm.me>
2026-04-11 23:46:57 -0600

    feat: add sequential CPU, pthreads, and OpenMP implementations

---
+++ bins/analyzer/src/sequentialCPU.cpp
@@ -0,0 +1,19 @@
+#include "sequentialCPU.hpp"
+
+#include <ctime>
+namespace sequentialCPU {
+
+void computeTimeStep(VectorField::FieldGrid& timeStep) {
+
+    const unsigned long numRow = timeStep.field.size();
+    const unsigned long numCol = timeStep.field[0].size();
+
+    for (unsigned long row = 0; row < numRow; row++) {
+        for (unsigned long col = 0; col < numCol; col++) {
+
+            timeStep.traceStreamlineStep(static_cast<int>(row), static_cast<int>(col));
+        }
+    }
+}
+
+} // namespace sequentialCPU
