d23b353551661c77662b1b0990c8eeb6528559ff
Rylei Mindrum <rymindrum@gmail.com>
2026-04-19 13:42:19 -0600

    typo

--- bins/analyzer/src/hybridCudaMpiStreamlineSolver.hpp
+++ bins/analyzer/src/hybridCudaMpiStreamlineSolver.hpp
@@ -2,11 +2,13 @@
 
 #include "streamlineSolver.hpp"
 
-class HybridCudaMpiStreamlineSolver : public streamlineSolver {
+class HybridCudaMpiStreamlineSolver : public StreamlineSolver {
   public:
-    explicit HybridCudaMpiStreamlineSolver(unsigned int blockSize 256) : blockSize_(blockSize) {}
+    explicit HybridCudaMpiStreamlineSolver(unsigned int blockSize = 256)
+        : blockSize_(blockSize) {}
+
     void computeTimeStep(Field::Grid& grid) override;
 
   private:
     unsigned int blockSize_;
-}
+};
